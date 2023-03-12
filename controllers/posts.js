ba286b305c70e7fd528b483497b185c6d027a50b
Amit Giri <thisisag840@gmail.com>
2023-03-12 22:03:16 +0530

    updatePost added

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+import express from 'express';
 import PostMessage from "../models/postMessage.js";
 
 
@@ -29,4 +31,13 @@ export const createPost = async (req,res) => {
         
     }
 
+}
+
+export const updatePost = async (req,res) => {
+    const { id:_id } = req.params;
+    const post = req.body;
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with this id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, {new: true});
+    res.json(updatedPost); 
 }
